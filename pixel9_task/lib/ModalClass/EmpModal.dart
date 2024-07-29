18af902bf7dbf1a7efa12da49c69fa2f7ed746d8
Khushalp <pkhushal684@gmail.com>
2024-07-29 23:55:30 +0530

    pixel6 task

---
+++ pixel9_task/lib/ModalClass/EmpModal.dart
@@ -0,0 +1,22 @@
+class Empmodal {
+  int id;
+  String image;
+  String firstName;
+  String lastName;
+  int age;
+  String gender;
+  String title;
+ String state;
+  String country;
+
+  Empmodal(
+      {required this.id,
+      required this.firstName,
+      required this.lastName,
+      required this.image,
+      required this.gender,
+      required this.age,
+      required this.title,
+      required this.state,
+      required this.country});
+}
